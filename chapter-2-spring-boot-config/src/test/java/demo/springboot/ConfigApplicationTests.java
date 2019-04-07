85dcc1a96206b8098f2956f31ed42c6fcbc9151e
skwqy <zhouf007@gmail.com>
2019-04-07 09:28:24 +0800

    leanring Springboot config

--- chapter-2-spring-boot-config/src/test/java/demo/springboot/ConfigApplicationTests.java
+++ chapter-2-spring-boot-config/src/test/java/demo/springboot/ConfigApplicationTests.java
@@ -2,9 +2,12 @@ package demo.springboot;
 
 import demo.springboot.config.BookComponent;
 import demo.springboot.config.BookProperties;
+import demo.springboot.config.UserProperties;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,22 +15,30 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ConfigApplicationTests {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(ConfigApplicationTests.class);
 	@Autowired
 	BookProperties bookProperties;
 
 	@Autowired
 	BookComponent bookComponent;
 
+	@Autowired
+	UserProperties userProperties;
+
 	@Test
 	public void testBookProperties() {
-		Assert.assertEquals(bookProperties.getName(),"[Spring Boot 2.x Core Action]");
-		Assert.assertEquals(bookProperties.getWriter(),"BYSocket");
+		Assert.assertEquals(bookProperties.getName(),"《Spring Boot 2.x 核心技术实战 - 上 基础篇》From YMAL");
+		Assert.assertEquals(bookProperties.getWriter(),"泥瓦匠BYSocket From YMAL");
 	}
 
 	@Test
 	public void testBookComponent() {
-		Assert.assertEquals(bookComponent.getName(),"[Spring Boot 2.x Core Action]");
-		Assert.assertEquals(bookComponent.getWriter(),"BYSocket");
+		Assert.assertEquals(bookComponent.getName(),"《Spring Boot 2.x 核心技术实战 - 上 基础篇》From YMAL");
+		Assert.assertEquals(bookComponent.getWriter(),"泥瓦匠BYSocket From YMAL");
+	}
+
+	@Test
+	public void randomTestUser() {
+		LOGGER.info("\n\n" + userProperties.toString() + "\n");
 	}
 }
